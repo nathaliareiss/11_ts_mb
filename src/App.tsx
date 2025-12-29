b05846cf413230af42b9c3c9442d967dd47daeb4
Nathalia <alencarnrr@gmail.com>
2025-12-29 12:43:11 -0300

    quebrando cabeca

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,41 @@ import SecondComponent from './components/SecondComponent';
 import Destructuring ,{Category} from './components/Destructuring';
 //aula useState no ts
 import State from './components/State';
+import { createContext } from 'react';
+//usando contexto
+import Context from './components/Context';
+
 
 //TYPES
 type textOrNull =string | null;
 
 
+//context
+interface IAppContext{
+  language:string;
+  framework:string;
+  projects:number;
+}
+
+export const AppContext = createContext<IAppContext | null>(null); 
 
 
 function App() {
 
-
 //types
-const myText:textOrNull = "Tem algum texto aqui"
-let mySecondText: textOrNull = null
+// const myText:textOrNull = "Tem algum texto aqui"
+// let mySecondText: textOrNull = null
+
+//context
+const contextValue ={
+  language:"JavaScript",
+  framework:"Express",
+  projects:5,
+};
+
 
   return (
+    <AppContext.Provider value={contextValue} >
     <div className="App">
     <h1>TS com re</h1>
 
@@ -31,7 +51,7 @@ let mySecondText: textOrNull = null
      title="Primeiro post"
      content="Algum conteudo"
      comentsQty={10}
-     tags={["ts,js"]}  
+     tags={["ts","js"]}  
     category={Category.P}
      /></div>
       <Destructuring 
@@ -40,11 +60,11 @@ let mySecondText: textOrNull = null
      comentsQty={10}
      tags={["Python"]}  
     category={Category.TS}
-
-   />
+/>
+  <Context />
     </div>
-   
+   </AppContext.Provider>
   );
+
 }
-   
 export default App;
